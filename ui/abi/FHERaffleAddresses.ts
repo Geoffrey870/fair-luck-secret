10bc1a10f9beb4b210ef3596455780132fccbe79
Geoffrey870 <LauraBetsyxupmi@outlook.com>
2025-12-07 05:08:04 +0800

    fix: improve error messages in UI components

---
+++ ui/abi/FHERaffleAddresses.ts
@@ -0,0 +1,9 @@
+/*
+  This file is auto-generated.
+  Command: 'npm run genabi'
+*/
+export const FHERaffleAddresses = {
+  "11155111": { address: "0xFfFC34216aCa5Ed8597AE3B1EEd884C19cfbD9FB", chainId: 11155111, chainName: "sepolia" },
+  "31337": { address: "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9", chainId: 31337, chainName: "hardhat" },
+};
+
